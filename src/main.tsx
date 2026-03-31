748dff604abfdb43d53194d1be7a032400546bf6
Stanislav Kucherenko <svkwork1@gmail.com>
2026-03-31 08:35:12 +0200

    add Mantine components and PostCSS configuration; update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 
+//mantime components
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+import {MantineProvider} from "@mantine/core";
+
+
 //Клиент для управления запросами
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -18,10 +25,12 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-              <App />
-          </BrowserRouter>
-      </QueryClientProvider>
+      <MantineProvider>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
+      </MantineProvider>
   </StrictMode>,
 )
